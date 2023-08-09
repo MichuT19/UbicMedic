9c8d877ec3b13941132eb84312f072a8cb493b97
michel <atenecelai@uteq.edu.ec>
2023-08-08 20:11:54 -0500

    models12

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class Sexoi(models.Model):
     descripcion = models.CharField(max_length=50,verbose_name = 'Sexo')
     class Meta:
         managed = True
-        db_table = 'Sexo'
+        db_table = 'Sexo1'
         verbose_name = 'Sexo'
         verbose_name_plural = 'Sexo'
     def __str__(self):
