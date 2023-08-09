11cf2b909b1b224252d68416d74293edb8689e2b
michel <atenecelai@uteq.edu.ec>
2023-08-08 20:05:10 -0500

    models

--- home/models.py
+++ home/models.py
@@ -1,7 +1,16 @@
 
+from django.db import models
 
-
-
+class Sexo(models.Model):
+    id_sexo = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=50,verbose_name = 'Sexo')
+    class Meta:
+        managed = True
+        db_table = 'Sexo'
+        verbose_name = 'Sexo'
+        verbose_name_plural = 'Sexo'
+    def __str__(self):
+        return self.descripcion
     
 
   
