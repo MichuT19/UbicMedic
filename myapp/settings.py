bda57437c1a28d3f8d5a4bfe317b2153082589fa
michel <atenecelai@uteq.edu.ec>
2023-08-12 11:33:53 -0500

    medi

--- myapp/settings.py
+++ myapp/settings.py
@@ -83,12 +83,22 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgresql://postgres:postgres@localhost:5432/mysite',
+#         conn_max_age=600
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ubicmedic_86cl',         # Nombre de la base de datos
+        'USER': 'ubicmedic_86cl_user',    # Nombre de usuario
+        'PASSWORD': 'KxEA32wjp2AVO1YHeaNWKTbsWUsBi1ty',  # Contraseña
+        'HOST': 'dpg-cj9i21m3ttrc73co1un0-a.oregon-postgres.render.com',  # Dirección del servidor
+        'PORT': '5432',                   # Puerto
+    }
 }
 
 # Password validation
@@ -122,6 +132,7 @@ USE_I18N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 
@@ -129,7 +140,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static')]
 
 if not DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
