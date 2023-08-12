027a9d7561e4c334db78370131f7caa53fa7f6ff
michel <atenecelai@uteq.edu.ec>
2023-08-12 10:00:51 -0500

    media22

--- myapp/settings.py
+++ myapp/settings.py
@@ -122,15 +122,14 @@ USE_I18N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static')]
 
 if not DEBUG:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
