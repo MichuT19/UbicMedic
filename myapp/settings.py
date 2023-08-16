119c9f397b96c350d0a94175638b719fb8c1a6fc
Orlando Cedeno <ocedenos@uteq.edu.ec>
2023-08-16 01:16:20 -0500

    probar fias

--- myapp/settings.py
+++ myapp/settings.py
@@ -139,6 +139,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static')]
 
