4fc025641d78ae5fd32181e920494c44c2d4b4a1
Orlando Cedeno <ocedenos@uteq.edu.ec>
2023-08-16 00:57:54 -0500

    probar imagensd

--- myapp/settings.py
+++ myapp/settings.py
@@ -134,14 +134,13 @@ USE_TZ = True
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# MEDIA_URL = 'static/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static')]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
