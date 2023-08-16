b5d68ac22476e9914a94e381c43ee79354b800df
Orlando Cedeno <ocedenos@uteq.edu.ec>
2023-08-16 00:31:48 -0500

    probar imagenes2

--- myapp/settings.py
+++ myapp/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = 'home/static/'
+MEDIA_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
