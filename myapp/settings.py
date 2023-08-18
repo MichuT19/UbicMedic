fc19d8b4fb68576b99462120e99f359eff38d263
michel <atenecelai@uteq.edu.ec>
2023-08-17 22:09:58 -0500

    nuevodeploy commit2

--- myapp/settings.py
+++ myapp/settings.py
@@ -157,22 +157,11 @@ if os.path.exists(f):
     sys.modules[module_name] = module
     exec(open(f, 'rb').read())
 
-cred = credentials.Certificate(os.path.join(PROJECT_APP, '../credenciales.json'))
+cred = credentials.Certificate(os.path.join(PROJECT_APP, './credenciales.json'))
 firebase_admin.initialize_app(cred)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# MEDIA_URL = 'static/images/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
-
-# Static files (CSS, JavaScript, Images)
-
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# MEDIA_URL = 'static/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'home','static')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home', 'static')]
